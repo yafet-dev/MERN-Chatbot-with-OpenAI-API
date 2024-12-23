fac54525eaf3844a5528daa5c2f3221ff4232bb2
minasie-ax <minasielux@gmail.com>
2024-12-23 23:11:04 +0100

    Added chat-bot-backend as a regular directory

---
+++ chat-bot-backend/src/user/user.resolver.ts
@@ -0,0 +1,13 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Query(() => [User], { name: 'users' })
+  findAll(): User[] {
+    return this.userService.findAll();
+  }
+}
